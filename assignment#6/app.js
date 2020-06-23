999cce24cd55bbcaccf81212ac5a5471a366855f
shehrozeahmed <57302393+shehrozeahmed@users.noreply.github.com>
2020-06-23 20:29:38 +0500

    Add files via upload
    chapter#26-30
    chapter#31-34

--- assignment#6/app.js
+++ assignment#6/app.js
@@ -1,127 +1,191 @@
-//  Chapters 21-25
-
-// task-01
-var firstName = prompt("Enter your first name: ");
-var lastName = prompt("Enter your last name: ");
-var fullName = firstName + " " + lastName;
-alert("Hello, " + fullName);
+//chapter 26-30
 
 
+// Task- 01
+var int = prompt("Enetr a positive Integer: ");
+document.write("Number: " + int + "<br>");
 
-//  task-02
-var favMobile = prompt("Enter the name of your favorite mobile model: ", "Samsung Galaxy S6 Edge");
-var favMobileLen = favMobile.length;
-document.write("<br> My favorite phone is: " + favMobile);
-document.write("<br> Length of string: " + favMobileLen);
+var round = Math.round(int)
+document.write("round off value: " + round + "<br>");
 
-//  task-03
-var myWord = "Pakistani";
-var indexOfn = myWord.indexOf("n");
-document.write("<br> String: " + myWord);
-document.write("<br> Index of 'n': " + indexOfn);
+var floor = Math.floor(int);
+document.write("Floor Value: " + floor + "<br>");
 
-//  task-04
-
-var Word = "Hello World";
-var indexOfLastL = Word.lastIndexOf("l");
-document.write("<br> String: " + Word);
-document.write("<br> Last index of 'l': " + indexOfLastL);
-
-// task-05
-
-var Word = "Pakistani";
-var indexOf3rdLetter = Word.charAt(3);
-document.write("<br> String: " + Word);
-document.write("<br> Character at index 3: " + indexOf3rdLetter);
-
-//task-06
-
-
-var firstName = prompt("Enter your first name: ");
-var lastName = prompt("Enter your last name: ");
-var fullName = firstName.concat(lastName);
-alert("Hello, " + fullName);
-
-
-// task-07
-
-var originalWord = "Hyderabad";
-var afterReplace = originalWord.replace("Hyder", "Islam");
-document.write("<br> City: " + originalWord);
-document.write("<br> After replacement: " + afterReplace);
-
-// task-08
-
-var message = "Ali and Sami are best friends. They play cricket and football together.";
-var updatedMessage = message.replace(/and/g, "&");
-document.write("<br> Original message: " + message);
-document.write("<br> Updated message: " + updatedMessage);
-
-
-//task-09
-
-var string = "472";
-var stringInNumber = parseInt(string);
-document.write("<br> Value: " + string);
-document.write("<br> Type: string");
-document.write("<br> Value: " + stringInNumber);
-document.write("<br> Type: number");
-
-
-//task-10
-
-var userInput = prompt("Enter the word: ");
-var userInputInUpperCase = userInput.toUpperCase();
-document.write("<br> User input: " + userInput);
-document.write("<br> Upper case: " + userInputInUpperCase);
-
-
-//task-12
-
-var num = 35.36;
-var numInString = num.toString().replace(".", "");
-document.write("<br> Number: " + num);
-document.write("<br> Result: " + numInString);
-
-//task-13
-
-var userName = prompt("Enter your name: ");
-if ((userName.indexOf("@") > 0) || (userName.indexOf(".") > 0) || (userName.indexOf(",") > 0) || (userName.indexOf("!") > 0)) {
-    alert("Please enter a valid username");
-}
-
-
-
-//task-14
+var ceil = Math.ceil(int);
+document.write("Ceil Value: " + ceil + "<br>")
 document.write("<br>")
-var a = ["cake", "apple pie", "cookie", "chips", "patties"]
-var item = prompt("welcome to ABC bakery.. what do you want sir/Madam??").toLowerCase()
-for (var i = 0; i < a.length; i++) {
-    if (item == a[i]) {
-        document.write(item + " is available at index " + a.indexOf(item) + " in our bakery");
-        break;
-    }
+
+
+// Task- 02
+var negativeFloat = prompt("Enetr a Negative Integer: ");
+document.write("Number: " + negativeFloat + "<br>");
+
+var round = Math.round(negativeFloat);
+document.write("round off value: " + round + "<br>");
+
+var floor = Math.floor(negativeFloat);
+document.write("Floor Value: " + floor + "<br>");
+
+var ceil = Math.ceil(negativeFloat);
+document.write("Ceil Value: " + ceil + "<br>")
+document.write("<br>")
+
+
+//Task- 03
+
+var num = prompt("Enter any Number: ");
+
+var absoluteValue = Math.abs(num);
+
+document.write("The absolute value of" + " " + num + "is" +absoluteValue )
+document.write("<br>")
+
+
+// Task- 04
+var dice = Math.random() * 6;
+var ceil = Math.ceil(dice);
+document.write("Random Dice Value: " + ceil + "<br>");
+document.write("<br>")
+
+
+//Task-05
+
+var toss = Math.random() * 2;
+var tossceil = Math.floor(toss);
+
+if (tossceil === 0) {
+    alert("Value " + tossceil + " has won the toss 'HEADS' ");
 }
-if (item !== a[i]) {
-    document.write("We are sorry, " + item + " is not available in our bakery")
+if (tossceil === 1) {
+    alert("Value " + tossceil + " has won the toss 'TAILS' ");
 }
-
-//task-15
-
-var university = "University of Karachi";
- /* to seperate each character of the string */
- var myArray = university.split("");
- document.write("<br>")
- for (var i = 0; i < myArray.length; i++){
-     document.write(myArray[i]);
-     document.write("<br>")
- }
+document.write("<br>")
 
 
-//  task-17
+//Task-06
 
-var input = prompt('Enter a city name');
- var lastindex = input.slice(-1);
-document.write(" User Input : "+ input + " <br> "+ "Last Character of input : " + lastindex);
+var numbers = Math.random() * 100;
+document.write("Random number between 1 to 100: " + Math.ceil(numbers) );
+document.write("<br>")
+
+
+//Task-07
+
+var kg = prompt("Enter your weight")
+kg = kg.toLowerCase()
+var weight = kg.split("kg")
+
+document.write("the weight of user is " + weight[0] + " Kilograms")
+document.write("<br>")
+
+
+//Task- 08
+
+var secretNumber = 9;
+var userInput = +prompt("Guess Number (ranging between 1 to 10 ) ");
+
+if (userInput === secretNumber) {
+    alert("Correct Answer.!");
+}
+else {
+    alert("Wrong.! Try Again...");
+}
+document.write("<br>")
+
+//chapter 31-34
+
+// Task: 01
+var date = new Date();
+document.write(date + "<br>");
+document.write("<br>")
+
+//Task-02
+
+var monthsNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var date = new Date();
+var currentMonth = date.getMonth();
+alert("Current Month: " + monthsNames[currentMonth]);
+document.write("<br>")
+
+
+// Task- 03
+var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+var day = date.getDay();
+var nameOfDay = days[day]
+document.write("Today is: " + nameOfDay + "<br>")
+document.write("<br>")
+
+
+//Task-04
+
+var dayNames = ['Sunday', 'Monday', 'Tueday', 'Wednesday', 'Thrday', 'Friday', 'Saturday'];
+var date = new Date();
+var today = date.getDay();
+if (dayNames[today] == 'Sunday' , dayNames[today]== "saturday") {
+    alert("It's Fun Day");
+}
+else{
+    alert("Its working day")
+}
+document.write("<br>")
+
+//Task- 05
+
+var date = new Date();
+var day = date.getDate();
+if (day < 16) {
+    alert("First fifteen days of the month");
+} else {
+    alert("Last days of the month" );
+
+}
+document.write("<br>")
+
+//Task-06
+
+var todayDate = new Date();
+document.write("Current Date: " + todayDate);
+var miliseconds = todayDate.getTime();
+document.write("<br>Elasped miliseconds since January 1, 1970: " + miliseconds);
+var minutes = miliseconds / (60 * 60);
+document.write("<br>Elasped minutes since January 1, 1970: " + minutes);
+document.write("<br>")
+
+//Task-07
+
+var todayDate = new Date();
+var time = todayDate.getHours();
+if (time >= 0 && time < 12) {
+    alert("It's AM");
+
+} else if (time >= 12 && time < 24) {
+    alert("It's PM");
+}
+document.write("<br>")
+
+//Task-08
+
+var date = new Date(2020, 11, 31);
+var laterDate = date
+document.write("Later date: " + date);
+document.write("<br>")
+
+
+//chapter 35-38
+
+//Task-01
+function date() {
+    var date = new Date();
+    return date;
+}
+document.write(date());
+
+//Task-02
+
+function GreetUser(Fname, Lname) {
+    var name = Fname + " " + Lname;
+    return name;
+}
+alert(GreetUser("Mohib","Ismail"))
 
 
